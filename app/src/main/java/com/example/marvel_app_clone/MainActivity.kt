f01159aefc2fa9a9998a5999fd4115b7db9ddea0
odley13 <franciscofrancieldo83@gmail.com>
2022-04-23 20:53:59 -0300

    commit \\MVVM\Android\app\java\MainActivity-erro no binding
    build (gradle-module/app) : Foi necessário colocar a dependencia do viewBinding para que binding funcionasse
    
    -Erro a activity, MainActivity.kt não estava criando o binding para importar automaticamente, foi verificado que na pasta generated o data_binding não havia sido criado.
    -Após realizar o invalidate caches, a dependencia do viewbinding voltou a funcionar.

--- app/src/main/java/com/example/marvel_app_clone/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app_clone/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.marvel_app_clone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.marvel_app_clone.databinding.ActivityMainBinding
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
