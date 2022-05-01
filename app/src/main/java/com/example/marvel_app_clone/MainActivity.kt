fb65486b429642d95ca2ebfadfbef9a78ca86563
odley13 <franciscofrancieldo83@gmail.com>
2022-05-01 14:58:47 -0300

    commit MVVM\Android\app\java\ui\MainActivity.kt
    feat (Implement navHost e navController) : Foi criado o naHost e o navController para que seja realizado o gerenciamento dos fragments, quando houve troca de tela.

--- app/src/main/java/com/example/marvel_app_clone/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app_clone/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.marvel_app_clone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.marvel_app_clone.databinding.ActivityMainBinding
 
 
@@ -10,11 +12,26 @@ import com.example.marvel_app_clone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navHostFragment : NavHostFragment
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+         val navController = navHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {  }
+        }
+
     }
 }
