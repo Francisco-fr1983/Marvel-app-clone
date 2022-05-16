f5697bc3f30d7bbff944aa947402b471827c9f36
odley13 <franciscofrancieldo83@gmail.com>
2022-05-15 22:13:32 -0300

    commit SplashActivity.kt
    refactory (Correção SplashScreen) : Feito algumas alterações no código, e colocado uma thread sleep na splash screen, para esperar 5 segundos antes de aparecer a tela inicial.
    
    Obs: No momento a splashScreen está iniciando com a tela em branco, verificando correção.

--- app/src/main/java/com/example/marvel_app_clone/SplashActivity.kt
+++ app/src/main/java/com/example/marvel_app_clone/SplashActivity.kt
@@ -8,8 +8,13 @@ class SplashActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Thread.sleep(5000)
+
         setContentView(R.layout.activity_splash)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         val view = binding.root
