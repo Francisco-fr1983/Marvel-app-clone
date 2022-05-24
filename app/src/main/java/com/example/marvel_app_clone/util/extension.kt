e029991cbfafeb55f2f918b54fc5efa07fdb8e99
odley13 <franciscofrancieldo83@gmail.com>
2022-05-24 10:14:07 -0300

    commit create no package util a extension.kt
    feat (create extension.kt) : Foi criado no package util, a extension.kt, com a função Fragment.toast para que seja chamada em todo o projeto.

---
+++ app/src/main/java/com/example/marvel_app_clone/util/extension.kt
@@ -0,0 +1,22 @@
+package com.example.marvel_app_clone.util
+
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+
+fun Fragment.toast(message: String, duration: Int = Toast.LENGTH_SHORT){
+    Toast.makeText(
+        requireContext(),
+        message,
+        duration
+    ).show()
+}
+
+fun View.show(){
+    visibility = View.VISIBLE
+}
+
+fun View.hide(){
+    visibility = View.INVISIBLE
+}
