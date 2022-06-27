d5c981218ac5eb9269f7c0d2157fb4b5401a03a2
odley13 <franciscofrancieldo83@gmail.com>
2022-06-27 19:25:32 -0300

    commit create clickAdapter
    feat (create clickAdapter) : Foi criado o clickAdapter, com a ação de clique na seta de ação.

--- app/src/main/java/com/example/marvel_app_clone/ui/list/ListCharacterFragment.kt
+++ app/src/main/java/com/example/marvel_app_clone/ui/list/ListCharacterFragment.kt
@@ -1,17 +1,45 @@
 package com.example.marvel_app_clone.ui.list
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.marvel_app_clone.databinding.FragmentListCharacterBinding
 import com.example.marvel_app_clone.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import com.example.marvel_app_clone.ui.adapters.CharacterAdapter
 
 
 @AndroidEntryPoint
 class ListCharacterFragment : BaseFragment<FragmentListCharacterBinding, ListCharacterViewModel>() {
 
     override val viewModel: ListCharacterViewModel by viewModels()
+    private val characterAdapter by lazy { CharacterAdapter() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        clickAdapter()
+    }
+
+    private fun clickAdapter() {
+        characterAdapter.setOnClickListener { characterModel ->
+            val action = ListCharacterFragmentDirections
+                    .actionListCharacterFragmentToDetailsCharacterFragment(characterModel)
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun setupRecyclerView() = with(binding) {
+        rvCharacters.apply {
+            adapter = characterAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+    //O onViewCreated é chamado depois do onCreate
 
 
     override fun getViewBinding(
