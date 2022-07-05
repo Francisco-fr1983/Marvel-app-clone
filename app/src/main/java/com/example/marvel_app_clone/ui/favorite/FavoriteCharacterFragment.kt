df73b122f5e70983bc90a384257fde7328071569
Odley13 <79156176+odley13@users.noreply.github.com>
2022-07-05 18:53:41 -0300

    commit feat FavoriteCharacterFragment
    feat FavoriteCharacterFragment -> Foi criado o FavoriteCharacterFragment com um AndroidEntryPoint, onde temos onViewCreated, com as funções setupRecuclerView, clickAdapter e o observer.

--- app/src/main/java/com/example/marvel_app_clone/ui/favorite/FavoriteCharacterFragment.kt
+++ app/src/main/java/com/example/marvel_app_clone/ui/favorite/FavoriteCharacterFragment.kt
@@ -1,20 +1,105 @@
 package com.example.marvel_app_clone.ui.favorite
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.marvel_app_clone.R
 import com.example.marvel_app_clone.ui.base.BaseFragment
 import com.example.marvel_app_clone.databinding.FragmentFavoriteCharacterBinding
+import com.example.marvel_app_clone.ui.adapters.CharacterAdapter
+import com.example.marvel_app_clone.ui.state.ResourceState
+import com.example.marvel_app_clone.util.hide
+import com.example.marvel_app_clone.util.show
+import com.example.marvel_app_clone.util.toast
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class FavoriteCharacterFragment: BaseFragment<FragmentFavoriteCharacterBinding, FavoriteCharacterViewModel>() {
 
     override val viewModel: FavoriteCharacterViewModel by viewModels()
+    private val characterAdapter by lazy {CharacterAdapter()}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        clickAdapter()
+        observer()
+    }
+
+    private fun observer() = lifecycleScope.launch {
+        viewModel.favorites.collect { resource ->
+            when(resource) {
+                is ResourceState.Sucess -> {
+                    resource.data?.let {
+                        binding.tvEmptyList.hide()
+                        characterAdapter.characters = it.toList()
+                    }
+
+                }
+                is ResourceState.Sucess -> {
+                    binding.tvEmptyList.show()
+
+                }
+                else ->{
+
+                }
+            }
+
+        }
+    }
+
+    private fun clickAdapter() {
+        characterAdapter.setOnClickListener { characterModel ->
+            val action = FavoriteCharacterFragmentDirections
+                .actionFavoriteCharacterFragmentToDetailsCharacterFragment(characterModel)
+            findNavController().navigate(action)
+
+
+        }
+    }
+
+    private fun setupRecyclerView() = with(binding) {
+        rvFavoriteCharacter.apply {
+            adapter = characterAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+        ItemTouchHelper(itemTouchHelperCallback()).attachToRecyclerView(rvFavoriteCharacter)
+    }
+
+    private fun itemTouchHelperCallback(): ItemTouchHelper.SimpleCallback{
+        return object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val character = characterAdapter.getCharacterPosition(viewHolder.adapterPosition)
+                viewModel.delete(character).also {
+                    toast(getString(R.string.message_delete_character))
+                }
+            }
+
+        }
+
+    }
 
 
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentFavoriteCharacterBinding = FragmentFavoriteCharacterBinding.inflate(inflater, container, false)
+
 }
