63c5e6a0422e47b58db451d9148221ee85a381c5
Odley13 <79156176+odley13@users.noreply.github.com>
2022-07-05 19:10:23 -0300

    commit feat Constants.kt
    feat Constants.kt -> Foi algumas constantes que serão usadas no projeto.

--- app/src/main/java/com/example/marvel_app_clone/util/Constants.kt
+++ app/src/main/java/com/example/marvel_app_clone/util/Constants.kt
@@ -1,9 +1,15 @@
 package com.example.marvel_app_clone.util
 
 object Constants {
+
+    const val DATABASE_NAME = "marvel.db"
     const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+
     const val PUBLIC_KEY = "a9d4fcda50825e5e4a8aa4f8773988f3"
     const val PRIVATE_KEY = "25454807ff9a0ef7c8f6940c6fc634efb15b2304"
+
+    const val LAST_SEARCH_QUERY = "last_search_query"
+    const val DEFAULT_QUERY = ""
 
     const val APIKEY = "apikey"
     const val HASH = "hash"
