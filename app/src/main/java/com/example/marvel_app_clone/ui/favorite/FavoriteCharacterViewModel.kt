77e1f8d4e85cb7a2126812bb34ad66ac91db83d4
Odley13 <79156176+odley13@users.noreply.github.com>
2022-07-05 18:57:09 -0300

    commit create FavoriteCharacterViewModel
    create FavoriteCharacterViewModel -> Foi criado o FavoriteCharacterViewModel com o Hilt, onde estanciamos o repository MarvelRepository e temos uma função delete.

--- app/src/main/java/com/example/marvel_app_clone/ui/favorite/FavoriteCharacterViewModel.kt
+++ app/src/main/java/com/example/marvel_app_clone/ui/favorite/FavoriteCharacterViewModel.kt
@@ -1,6 +1,38 @@
 package com.example.marvel_app_clone.ui.favorite
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.marvel_app_clone.data.model.character.CharacterModel
+import com.example.marvel_app_clone.repository.MarvelRepository
+import com.example.marvel_app_clone.ui.state.ResourceState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class FavoriteCharacterViewModel: ViewModel() {
+@HiltViewModel
+class FavoriteCharacterViewModel @Inject constructor(
+    private val repository: MarvelRepository
+) : ViewModel() {
+    private val _favorites = MutableStateFlow<ResourceState<List<CharacterModel>>>(ResourceState.Empty())
+    val favorites: StateFlow<ResourceState<List<CharacterModel>>> = _favorites
+
+    init {
+        fetch()
+    }
+
+    private fun fetch() = viewModelScope.launch {
+        repository.getAll().collectLatest { characters ->
+            if(characters.isNullOrEmpty()){
+                _favorites.value = ResourceState.Empty()
+            }else{
+                _favorites.value = ResourceState.Sucess(characters)
+            }
+        }
+    }
+    fun delete(characterModel: CharacterModel) = viewModelScope.launch {
+        repository.delete(characterModel)
+    }
 }
