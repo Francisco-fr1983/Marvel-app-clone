bd4c1bab336e1fbe605711113162a17dd2b40e5f
Odley13 <79156176+odley13@users.noreply.github.com>
2022-07-05 19:00:31 -0300

    commit feat ListCharacterFragment.kt
    feat ListCharacterFragment.kt -> Foi o ListCharacterFragment.kt com um @AndroidEntryPoint, criado a função onViewCreated, com suas funções setupRecuclerView, clickAdapter e o collectObserver.

--- app/src/main/java/com/example/marvel_app_clone/ui/list/ListCharacterFragment.kt
+++ app/src/main/java/com/example/marvel_app_clone/ui/list/ListCharacterFragment.kt
@@ -5,12 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.marvel_app_clone.R
 import com.example.marvel_app_clone.databinding.FragmentListCharacterBinding
-import com.example.marvel_app_clone.ui.base.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
 import com.example.marvel_app_clone.ui.adapters.CharacterAdapter
+import com.example.marvel_app_clone.ui.base.BaseFragment
+import com.example.marvel_app_clone.ui.state.ResourceState
+import com.example.marvel_app_clone.util.hide
+import com.example.marvel_app_clone.util.show
+import com.example.marvel_app_clone.util.toast
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.util.*
 
 
 @AndroidEntryPoint
@@ -23,15 +33,46 @@ class ListCharacterFragment : BaseFragment<FragmentListCharacterBinding, ListCha
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView()
         clickAdapter()
+        collectObserver()
+    }
+
+    private fun collectObserver() = lifecycleScope.launch {
+        viewModel.list.collect { resource ->
+            when (resource) {
+                is ResourceState.Sucess -> {
+                    resource.data?.let { values ->
+                        binding.progressCircular.hide()
+                        characterAdapter.characters = values.data.results.toList()
+                    }
+
+                }
+                is ResourceState.Error -> {
+                    binding.progressCircular.hide()
+                    resource.message?.let { message ->
+                        toast(getString(R.string.an_error_occurred))
+                        Timber.tag("ListCharacterFragment").e("Error -> $message")
+                    }
+
+                }
+                is ResourceState.Loading -> {
+                    binding.progressCircular.show()
+
+                }
+                else -> {
+                }
+            }
+
+        }
     }
 
     private fun clickAdapter() {
-        characterAdapter.setOnClickListener { characterModel ->
+        characterAdapter.setOnClickListener {  characterModel ->
             val action = ListCharacterFragmentDirections
-                    .actionListCharacterFragmentToDetailsCharacterFragment(characterModel)
+                .actionListCharacterFragmentToDetailsCharacterFragment(characterModel)
             findNavController().navigate(action)
         }
     }
+
 
     private fun setupRecyclerView() = with(binding) {
         rvCharacters.apply {
@@ -45,5 +86,7 @@ class ListCharacterFragment : BaseFragment<FragmentListCharacterBinding, ListCha
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): FragmentListCharacterBinding = FragmentListCharacterBinding.inflate(inflater, container, false)
+    ): FragmentListCharacterBinding =
+        FragmentListCharacterBinding.inflate(inflater, container, false)
 }
+
