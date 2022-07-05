d89cf5af49509a53771b905d35e912e9bf1531be
Odley13 <79156176+odley13@users.noreply.github.com>
2022-07-05 19:28:26 -0300

    commit create MarvelDatabase.kt
    create MarvelDatabase.kt -> Foi criado o MarvelDatabase.kt, que será o banco de dados interno para salvar os favoritos no banco.

---
+++ app/src/main/java/com/example/marvel_app_clone/data/MarvelDatabase.kt
@@ -0,0 +1,12 @@
+package com.example.marvel_app_clone.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.marvel_app_clone.data.model.character.CharacterModel
+
+@Database(entities = [CharacterModel::class], version = 1, exportSchema = false)
+@TypeConverters(MarvelConverters::class)
+abstract class MarvelDatabase :  RoomDatabase() {
+    abstract fun marvelDao(): MarvelDao
+}
