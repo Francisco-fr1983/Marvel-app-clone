af910d8b3488841bdf567636048b449444e6c093
Odley13 <79156176+odley13@users.noreply.github.com>
2022-07-05 19:35:10 -0300

    commit refactor MarvelRepository.kt
    refactor MarvelRepository.kt -> Foi refactorado o MarvelRepository.kt, para ceder as instancias da API e DAO, assim como as funções de inserir, deletar, listar e buscar.

--- app/src/main/java/com/example/marvel_app_clone/repository/MarvelRepository.kt
+++ app/src/main/java/com/example/marvel_app_clone/repository/MarvelRepository.kt
@@ -1,12 +1,19 @@
 package com.example.marvel_app_clone.repository
 
+import com.example.marvel_app_clone.data.local.MarvelDao
+import com.example.marvel_app_clone.data.model.character.CharacterModel
 import com.example.marvel_app_clone.data.remote.ServiceApi
 import javax.inject.Inject
 
 
 class MarvelRepository @Inject constructor(
-        private val api: ServiceApi
+        private val api: ServiceApi,
+        private val dao: MarvelDao
 ) {
     suspend fun list(nameStartsWith: String? = null) = api.list(nameStartsWith)
     suspend fun getComics(CharacterId: Int) = api.getComics(CharacterId)
+
+    suspend fun insert(characterModel: CharacterModel) = dao.insert(characterModel)
+    fun getAll() = dao.getAll()
+    suspend fun delete(characterModel: CharacterModel) = dao.delete(characterModel)
 }
