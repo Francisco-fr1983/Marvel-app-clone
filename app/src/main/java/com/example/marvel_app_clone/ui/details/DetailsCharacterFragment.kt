db4341734e864d4eb13ce8a2ed8237a337753b14
Odley13 <79156176+odley13@users.noreply.github.com>
2022-07-05 18:45:36 -0300

    commit feat DetailsCharacterFragment.kt
    feat DetailsCharacterFragment.kt -> Feito o completo do DetailsCharacterFragment.kt, instanciando o comicAdapter e characterModel, criado a onViewCreated e suas funções setupRecyclerView, onLoadedCharacter e o collectObserver onde abrimos a corroutines e pegamos o viewModel para mostrar os ResourceState do result.

--- app/src/main/java/com/example/marvel_app_clone/ui/details/DetailsCharacterFragment.kt
+++ app/src/main/java/com/example/marvel_app_clone/ui/details/DetailsCharacterFragment.kt
@@ -1,20 +1,139 @@
 package com.example.marvel_app_clone.ui.details
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.example.marvel_app_clone.R
+import com.example.marvel_app_clone.data.model.character.CharacterModel
 import com.example.marvel_app_clone.ui.base.BaseFragment
 import com.example.marvel_app_clone.databinding.FragmentDetailsCharacterBinding
+import com.example.marvel_app_clone.ui.adapters.ComicAdapter
+import com.example.marvel_app_clone.ui.state.ResourceState
+import com.example.marvel_app_clone.util.hide
+import com.example.marvel_app_clone.util.limitDescription
+import com.example.marvel_app_clone.util.show
+import com.example.marvel_app_clone.util.toast
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class DetailsCharacterFragment : BaseFragment<FragmentDetailsCharacterBinding, DetailsCharacterViewModel>() {
 
     override val viewModel: DetailsCharacterViewModel by viewModels()
 
+    private val args: DetailsCharacterFragmentArgs by navArgs()
+    private val comicAdapter by lazy { ComicAdapter() }
+    private lateinit var characterModel: CharacterModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        characterModel = args.character
+        setupRecyclerView()
+        onLoadedCharacter(characterModel)
+        collectObserver()
+        descriptionCharacter()
+    }
+
+    private fun descriptionCharacter() {
+        binding.tvDescriptionCharacterDetails.setOnClickListener {
+            onShowDialog(characterModel)
+        }
+    }
+
+    private fun onShowDialog(characterModel: CharacterModel) {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(characterModel.name)
+            .setMessage(characterModel.description)
+            .setNegativeButton(getString(R.string.close_dialog)) {dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun collectObserver() = lifecycleScope.launch {
+        viewModel.details.collect { result ->
+            when(result) {
+                is ResourceState.Sucess -> {
+                    binding.progressBarDetail.hide()
+                    result.data?.let { values ->
+                        if (values.data.result.count() > 0) {
+                            comicAdapter.comics = values.data.result.toList()
+                    }else{
+                        toast(getString(R.string.empty_list_comics))
+
+                      }
+                  }
+                }
+                is ResourceState.Error -> {
+                    binding.progressBarDetail.hide()
+                    result.message?.let { message ->
+                        Timber.tag("DetailsCharacter").e("Error -> $message")
+                        toast(message)
+                    }
+
+                }
+                is ResourceState.Loading -> {
+                    binding.progressBarDetail.show()
+
+
+                }
+                else ->{}
+            }
+        }
+    }
+
+    private fun onLoadedCharacter(characterModel: CharacterModel) = with(binding){
+        tvNameCharacterDetails.text = characterModel.name
+        if(characterModel.description.isEmpty()) {
+            tvDescriptionCharacterDetails.text =
+                requireContext().getString(R.string.text_description_empty)
+        }else{
+            tvDescriptionCharacterDetails.text = characterModel.description.limitDescription(100)
+        }
+        Glide.with(requireContext())
+            .load(characterModel.thumbnail.path + "." + characterModel.thumbnail.extension)
+            .into(imgCharacterDetails)
+    }
+
+    private fun setupRecyclerView() = with(binding){
+        rvComics.apply {
+            adapter = comicAdapter
+            viewModel.fetch(characterModel.id)
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_details, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.favorite -> {
+                viewModel.insert(characterModel)
+                toast(getString(R.string.saved_successfully))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentDetailsCharacterBinding = FragmentDetailsCharacterBinding.inflate(inflater, container, false)
+
 }
